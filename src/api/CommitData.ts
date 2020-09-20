ca1a894866479a8b8025c48c325b62d79e4502fc
Spencer Shadley <spencershadley@utexas.edu>
2020-09-20 12:07:24 -0700

    added commitdata

--- src/api/CommitData.ts
+++ src/api/CommitData.ts
@@ -1,4 +1,7 @@
+import { AuthorData } from "./AuthorData";
+
 export interface CommitData {
+    author: AuthorData,
     comments_url: string,
     // commit: {author: {name: "David Liu", email: "qiang.david.liu@gmail.com", date: "2018-11-19T22:18:19Z"},…}
     // committer: {login: "web-flow", id: 19864447, node_id: "MDQ6VXNlcjE5ODY0NDQ3",…}
