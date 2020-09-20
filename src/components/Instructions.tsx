9c4a51cac0bd612ce74e22bb07f5ef78ead759f0
Spencer Shadley <spencershadley@utexas.edu>
2020-09-20 15:59:42 -0700

    added reactstrap and jumbotron instructions

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,14 +1,16 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
+import { Container } from "react-bootstrap";
+import Jumbotron from "react-bootstrap/esm/Jumbotron";
 
 export class Instructions extends React.Component {
     render() {
         return (
-            <div>
-                <Typography variant='h3'>
-                        Use the above bar to search for an organization on GitHub!
-                    </Typography>
-            </div>
+            <Jumbotron fluid>
+                <Container>
+                    <h1>GitHub Org Searcher</h1>
+                    <p>Use the above bar to search for an organization on GitHub!</p>
+                </Container>
+            </Jumbotron>
             );
     }
 }
