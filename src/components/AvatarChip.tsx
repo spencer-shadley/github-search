a7226226e6328eef53a8e1479080ffd7cd2418c5
Spencer Shadley <spencershadley@utexas.edu>
2020-09-20 12:52:37 -0700

    added grow transition

--- src/components/AvatarChip.tsx
+++ src/components/AvatarChip.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Chip } from "@material-ui/core";
+import { Avatar, Chip, Grow } from "@material-ui/core";
 import React from "react";
 import { CommitData } from "../api/CommitData";
 
@@ -9,13 +9,15 @@ interface AvatarChipProps {
 export class AvatarChip extends React.Component<AvatarChipProps> {
     render() {
         return (
-            <Chip
-                clickable={!!this.props.data?.author?.html_url}
-                href={this.props.data?.author?.html_url}
-                component={'a'}
-                target={'_blank'}
-                avatar={<Avatar src={this.props.data?.author?.avatar_url} />}
-                label={this.props.data?.author?.login || 'Unknown'}/>
+            <Grow in={true}>
+                <Chip
+                    clickable={!!this.props.data?.author?.html_url}
+                    href={this.props.data?.author?.html_url}
+                    component={'a'}
+                    target={'_blank'}
+                    avatar={<Avatar src={this.props.data?.author?.avatar_url} />}
+                    label={this.props.data?.author?.login || 'Unknown'}/>
+            </Grow>
         );
     }
 }
