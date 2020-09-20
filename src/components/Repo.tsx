464f1873790e636e03e063f8b43ad7a0513df871
Spencer Shadley <spencershadley@utexas.edu>
2020-09-20 13:36:30 -0700

    added commit error handling

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,9 +1,8 @@
-import { LinearProgress } from "@material-ui/core";
+import { LinearProgress, Typography } from "@material-ui/core";
 import React from "react";
 import { CommitData } from "../api/CommitData";
 import { findCommits } from "../api/github";
 import { RepoData } from "../api/RepoData";
-import { Commit } from "./Commit";
 import { Commits } from "./Commits";
 
 interface RepoProps {
@@ -12,31 +11,41 @@ interface RepoProps {
 
 interface RepoState {
     commits: CommitData[]
+    errorText: string
 }
 
 export class Repo extends React.Component<RepoProps, RepoState> {
     constructor(props: RepoProps) {
         super(props);
         this.state = {
-            commits: []
+            commits: [],
+            errorText: ''
         }
     }
 
     componentDidMount() {
-        findCommits(this.props.repoData.owner.login, this.props.repoData.name).then(res => {
-            this.setState({
-                commits: res.data
+        findCommits(this.props.repoData.owner.login, this.props.repoData.name)
+            .then(res => {
+                this.setState({
+                    commits: res.data
+                })
             })
-        })
+            .catch(err => {
+                console.error(err);
+                this.setState({
+                    errorText: `${err}: ${err.response.statusText}`
+                })
+            })
     }
 
     render() {
         return (
             <div>
-                {this.props.repoData.full_name}
                 {this.state.commits.length > 0 ?
                     <Commits data={this.state.commits} /> :
-                    <LinearProgress/>
+                    this.state.errorText ?
+                        <Typography>{this.state.errorText}</Typography> :
+                        <LinearProgress/>
                 }
             </div>
             );
